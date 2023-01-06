45a44a32df011d4b920a5361acf998dbf32bb172
Carol Pinheiro <caroline@bossabox.com>
2023-01-06 16:01:05 -0300

    feat: created way to receive data through udp

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 import fs from "fs";
 import bencode from "bencode";
 
+import dgram from "dgram";
+import { Buffer } from "buffer";
+import { parse as urlParse } from "url";
+
 const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
-console.log(torrent.announce.toString("utf8"));
+const url = urlParse(torrent.announce.toString("utf8"));
+const socket = dgram.createSocket("udp4");
+const myMsg = Buffer.from("hello?", "utf-8");
+
+socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
+
+socket.on("message", (msg) => {
+  console.log("message is", msg);
+});
