ae571432fc7c9f98493aee415a4ec5e8bf308001
Carol Pinheiro <caroline@bossabox.com>
2023-01-06 15:21:39 -0300

    feat: decoded buffer to bencode and retrieved track string

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+import fs from "fs";
+import bencode from "bencode";
+
+const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
+console.log(torrent.announce.toString("utf8"));
