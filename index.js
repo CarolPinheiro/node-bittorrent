c2ec4106fa996190710f5c2239011aaae85bea88
Carol Pinheiro <caroline@bossabox.com>
2023-01-09 10:53:12 -0300

    chore: updated imports

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 "use strict";
 import fs from "fs";
 import bencode from "bencode";
+import * as tracker from "./tracker.js";
+import * as torrentParser from "./torrent-parser";
 
-import dgram from "dgram";
-import { Buffer } from "buffer";
-import { parse as urlParse } from "url";
+const torrent = torrentParser.open("puppy.torrent");
 
-const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
-const url = urlParse(torrent.announce.toString("utf8"));
-const socket = dgram.createSocket("udp4");
-const myMsg = Buffer.from("hello?", "utf-8");
-
-socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
-
-socket.on("message", (msg) => {
-  console.log("message is", msg);
+tracker.getPeers(torrent, (peers) => {
+  console.log("list of peers: ", peers);
 });
